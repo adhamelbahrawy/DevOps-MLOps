5f12fe70dfa254dd26c5e43b241c60ca8323a513
adhamelbahrawy <adhamelbahrawie@gmail.com>
2024-04-23 17:23:38 +0200

    Début exo 3

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,19 +6,34 @@
 # }
 
 # Exercice 2
-variable "movie" {
-  type        = string
-  default     = "film quelconque"
+# variable "movie" {
+#   type        = string
+#   default     = "film quelconque"
+# }
+
+# resource "null_ressource" "Exo2" {
+#   provisioner "local-exec" {
+#     command = "echo '${var.movie}' >> film.txt"
+#   }
+# }
+
+# output "Confirmation" {
+#   value = "Fichier film.txt créer"
+# }
+
+# Exercice 3
+variable "movies_name" {
+  type = map(string)
+  default = {
+    film1  = "Nom film1"
+    film2  = "Nom film2"
+    film3 = "Nom film3"
+  }
 }
 
 resource "null_ressource" "Exo2" {
   provisioner "local-exec" {
-    command = "echo '${var.movie}' >> film.txt"
+    for_each = var.movies_name
+    command = "echo '${var.movie}' >> Movies.txt"
   }
 }
-
-output "Confirmation" {
-  value = "Fichier film.txt créer"
-}
-
-# Exercice 3
