c59a6c968ee097fdb00fd60abd8e3ebfab8245e3
adhamelbahrawy <adhamelbahrawie@gmail.com>
2024-04-23 16:43:12 +0200

    exo 2 terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,26 +1,24 @@
-resource "terraform_data" "Exo1" {
+# Exercice 1
+# resource "null_ressource" "Exo1" {
+#   provisioner "local-exec" {
+#     command = "echo 'Nom du film' >> film.txt"
+#   }
+# }
+
+# Exercice 2
+variable "movie" {
+  type        = string
+  default     = "film quelconque"
+}
+
+resource "null_ressource" "Exo2" {
   provisioner "local-exec" {
-    command = "echo 'Nom du film' >> film.txt"
+    command = "echo '${var.movie}' >> film.txt"
   }
 }
 
-# Exercice 2
-# Le but de cet exercice est de créer une configuration Terraform (main.tf) qui écrit le nom de votre film préféré dans le fichier film.txt le nom du film doit être contenu dans une variable nommé movie_name.
-# De plus vous devez ajouté un output « Fichier film.txt créer ».
-# Utilisation d’une variable de type String.
-# variable name {
-#   type        = string
-#   default     = "film quelconque"
-# }
+output "Confirmation" {
+  value = "Fichier film.txt créer"
+}
 
-# resource "terraform_data" "Exo2" {
-#   movie_name = "Nom du film"
-#   provisioner "local-exec" {
-#     command = "echo >> film.txt"
-#   }
-#   provisioner "local-exec" {
-#     command = "echo >> film.txt"
-#   }
-# }
-# }
-
+# Exercice 3
